5c475239f97c1293cf661b6dd797260ad3860446
samira-2002-code <samiraassemlali2@gmail.com>
2026-04-24 09:47:37 +0100

    close statement and database connection

--- scripts/authprocess.php
+++ scripts/authprocess.php
@@ -13,7 +13,11 @@ if (empty($nom) || empty($prenom) || empty($email) || empty($password)) {
    exit(); 
 }
 
-
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    header("Location: ../public/register.php?error=invalidemail");
+    
+    exit();
+}
 
 
 ?>
